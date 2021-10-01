acd2ac91e81ca5b1357643879d11af7d6f11f748
Harrisonp365 <harrisonpowell365@gmail.com>
2021-10-01 15:13:04 +1000

    had lots of issues with html and css, so I have just made it a bit more simple for now till I get it all working then will focus on styling as its not my strong point

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,64 @@
-const btn = document.getElementsByClassName("btn");
-const display = document.getElementsByClassName("calculator__display");
-const body = document.getElementById("body");
+const previousValue = document.querySelector(".previousVal");
+const currentValue = document.querySelector(".currentVal");
+
+const clear = document.querySelector(".clear");
+const allClear = document.querySelector(".allClear");
+const equals = document.querySelector(".equals");
+
+// I feel like class makes sense for all the functions needed
+class Calculator {
+    constructor() {
+        
+    }
+
+    clear() {
+        //current and prev opperand = '';
+    }
+
+    delete() {
+
+    }
+
+    appendNum(num) {
+
+    }
+
+    opperation(operator) {
+
+    }
+
+    total() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+
+const calculator = new Calculator();
+const numButtons = document.querySelectorAll(".number");
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNum(button.innerText);
+        //calculator.updateDisplay();
+    });
+});
+
+const opButtons = document.querySelectorAll(".operator");
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.opperation(button.innerText);
+        //calculator.updateDisplay();
+    });
+});
+
 
 //on click we want to recieve the value either in an array or just as a number / possibly multi dimensional arr for use with order of opperation
 //once recieved I need to display each num or operator in .display
 
 //research btn value, This only pases a string value so I will need to parse to int, I also need to work out class names
-
-console.log(btn);
-
-btn.addEventListeners("click", () => {
-    //need to use forEach here to add listener to all items
-    body.classList.toggle("body--red");
-});
 
 // const appendChild = (elemType, input, parentNode) => {
 //     const newElem = document.createElement(elemType);
